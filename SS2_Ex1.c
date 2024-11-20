b56a483c4ea5c9a0c8834cbd84aca8b6f84bd347
Breaveluv <nam.bule12@gmail.com>
2024-11-20 21:13:48 +0700

    Add files via upload

---
+++ SS2_Ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdio.h>
+
+int main(){
+	 // kieu du lieu so nguyen 
+	 int vu_hoai_nam_2006;
+	 // kieu du lieu so thuc
+	 float chelsea_so_1;
+	 double mu_day_xa_hoi;
+	 // kieu du lieu ki tu 
+	 char thay_bach_fan_liverpool;
+
+	return 0;
+} 
---
+++ SS2_Ex2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	 int c = 4,b = 6;
+	 printf("so thu nhat la : %d \n",c);
+	 printf("so thu hai la : %d \n",b);
+	 int d = c + b;
+	 printf("tong hai so la : %d \n",d);
+	
+	return 0;
+} 
