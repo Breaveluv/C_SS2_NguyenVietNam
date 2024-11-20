fcd65fe26c73c6c18bde2998e4e84af1cc8923c9
Breaveluv <nam.bule12@gmail.com>
2024-11-20 21:33:44 +0700

    Add files via upload

---
+++ SS2_Ex5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	  int c = 8 , d = 6;
+	  printf("chieu dai la : %d \n",c);
+	  printf("chieu rong la : %d \n",d);
+	  int e = ( c + d ) * 2;
+	  printf("chu vi la : %d \n",e);
+	  int f = c * d;
+	  printf("dien tich la : %d \n",f);
+	
+	return 0;
+} 
